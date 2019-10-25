250efe49b19ea2c672087c41d89bcf65131d2ba6
itwysgsl <iamstenman@protonmail.com>
2019-10-25 16:32:31 +0900

    Genesis transaction cache

--- core/rest.py
+++ core/rest.py
@@ -6,6 +6,7 @@ from methods.block import Block
 import core.utils as utils
 from flask import Response
 import requests
+import json
 
 def init(api):
 	api.add_resource(GetInfo, '/info')
@@ -82,7 +83,13 @@ class BlockHeader(Resource):
 
 class TransactionInfo(Resource):
 	def get(self, thash):
-		return Transaction().info(thash)
+		if thash == '3426ccad3017e14a4ab6efddaa44cb31beca67a86c82f63de18705f1b6de88df':
+			with open('cache/genesis.json') as file:
+				data = json.load(file)
+
+			return data
+		else:
+			return Transaction().info(thash)
 
 class AddressBalance(Resource):
 	def get(self, address):
