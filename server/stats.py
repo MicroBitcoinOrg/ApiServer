c8398c76f718b2c9834382c21590b9e76beb305a
itwysgsl <iamstenman@protonmail.com>
2019-11-28 02:12:22 +0900

    Stats code cleanup

--- server/stats.py
+++ server/stats.py
@@ -1,47 +1,32 @@
-from server import socket_counter
-from server import rest_counter
 from datetime import timedelta
-from server import subscribers
-from server import connections
-from server import start_time
-from server import rooms
-import server
+import server as state
 import time
 
 def socket(func):
 	def wrapper(*args, **kwargs):
-		global socket_counter
-		socket_counter += 1
+		state.socket_counter += 1
 		return func(*args, **kwargs)
 
 	return wrapper
 
 def rest(func):
 	def wrapper(*args, **kwargs):
-		global rest_counter
-		rest_counter += 1
+		state.rest_counter += 1
 		return func(*args, **kwargs)
 
 	return wrapper
 
 def info():
-	global socket_counter
-	global rest_counter
-	global subscribers
-	global connections
-	global start_time
-	global rooms
-
-	uptime = timedelta(seconds=time.monotonic() - start_time)
+	uptime = timedelta(seconds=time.monotonic() - state.start_time)
 	return {
 		'uptime': str(uptime),
 		'subscriptions': {
-			'connections': server.connections,
-			'subscribers': len(subscribers),
-			'rooms': len(rooms)
+			'connections': state.connections,
+			'subscribers': len(state.subscribers),
+			'rooms': len(state.rooms)
 		},
 		'requests': {
-			'socket': socket_counter,
-			'rest': rest_counter
+			'socket': state.socket_counter,
+			'rest': state.rest_counter
 		}
 	}
--- server/subscription.py
+++ server/subscription.py
@@ -1,22 +1,14 @@
 from server.methods.transaction import Transaction
 from server.methods.general import General
 from server.methods.block import Block
-from server import subscribers
-from server import mempool
 from flask import request
-from server import thread
 from server import stats
 from server import utils
-from server import rooms
 from server import sio
 import server as state
 import flask_socketio
 
 def init():
-	global subscribers
-	global mempool
-	global rooms
-
 	bestblockhash = None
 
 	while True:
@@ -30,8 +22,8 @@ def init():
 
 			updates = Block().inputs(bestblockhash)
 			for address in updates:
-				mempool = list(set(mempool) - set(updates[address]))
-				if address in rooms:
+				mempool = list(set(state.mempool) - set(updates[address]))
+				if address in state.rooms:
 					sio.emit('address.update', utils.response({
 						'address': address,
 						'tx': updates[address],
@@ -45,7 +37,7 @@ def init():
 		for address in updates:
 			updates[address] = list(set(updates[address]) - set(mempool))
 			temp_mempool += updates[address]
-			if address in rooms:
+			if address in state.rooms:
 				if len(updates[address]) > 0:
 					sio.emit('address.update', utils.response({
 						'address': address,
@@ -59,45 +51,36 @@ def init():
 @stats.socket
 @sio.on('connect')
 def user_connect():
-	global thread
-
 	state.connections += 1
-	if thread is None:
-		thread = sio.start_background_task(target=init)
+	if state.thread is None:
+		state.thread = sio.start_background_task(target=init)
 
 @stats.socket
 @sio.on('disconnect')
 def user_disconnect():
-	global connections
-	global subscribers
-	global rooms
-
 	state.connections -= 1
-	if request.sid in subscribers:
-		for room in subscribers[request.sid]:
-			if request.sid in rooms[room]:
-				rooms[room].remove(request.sid)
+	if request.sid in state.subscribers:
+		for room in state.subscribers[request.sid]:
+			if request.sid in state.rooms[room]:
+				state.rooms[room].remove(request.sid)
 				flask_socketio.leave_room(room, request.sid)
-				if len(rooms[room]) == 0:
-					rooms.pop(room)
+				if len(state.rooms[room]) == 0:
+					state.rooms.pop(room)
 
-		subscribers.pop(request.sid)
+		state.subscribers.pop(request.sid)
 
 @stats.socket
 @sio.on('subscribe.blocks')
 def user_subscribe_blocks():
-	global subscribers
-	global rooms
+	if request.sid not in state.subscribers:
+		state.subscribers[request.sid] = []
 
-	if request.sid not in subscribers:
-		subscribers[request.sid] = []
-
-	if 'blocks' not in rooms:
-		rooms['blocks'] = [request.sid]
+	if 'blocks' not in state.rooms:
+		state.rooms['blocks'] = [request.sid]
 	else:
-		rooms['blocks'].append(request.sid)
+		state.rooms['blocks'].append(request.sid)
 
-	subscribers[request.sid].append('blocks')
+	state.subscribers[request.sid].append('blocks')
 	flask_socketio.join_room('blocks', request.sid)
 
 	return True
@@ -105,18 +88,15 @@ def user_subscribe_blocks():
 @stats.socket
 @sio.on('subscribe.address')
 def user_subscribe_address(address):
-	global subscribers
-	global rooms
+	if request.sid not in state.subscribers:
+		state.subscribers[request.sid] = []
 
-	if request.sid not in subscribers:
-		subscribers[request.sid] = []
-
-	if address not in rooms:
-		rooms[address] = [request.sid]
+	if address not in state.rooms:
+		state.rooms[address] = [request.sid]
 	else:
-		rooms[address].append(request.sid)
+		state.rooms[address].append(request.sid)
 
-	subscribers[request.sid].append(address)
+	state.subscribers[request.sid].append(address)
 	flask_socketio.join_room(address, request.sid)
 
 	return True
