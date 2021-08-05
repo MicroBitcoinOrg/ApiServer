f1badf4025d4475c4d48b2ebdeeadb91b15ed736
DavidJack01 <widecoin.official@gmail.com>
2021-08-05 07:47:24 +0000

    Updated Gecko API

--- server/utils.py
+++ server/utils.py
@@ -78,7 +78,7 @@ def supply(height):
         supply += total
 
     supply = supply + height * reward"""
-    #logger.info(sub_total_supply)
+    #logger.info(sub_total_supplyd)
     return {
         "halvings": int(halvings_count),
         #"supply": int(supply)
